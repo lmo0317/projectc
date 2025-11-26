17b96dd6d7161f98b0b4482aab1f1101a58e03ed
lmo0317 <lmo0317@naver.com>
2025-11-26 16:44:01 +0900

    TASK-001

---
+++ Assets/Scripts/Systems/TestSystem.cs
@@ -0,0 +1,13 @@
+using Unity.Entities;
+
+/// <summary>
+/// DOTS 환경 검증용 테스트 시스템
+/// TASK-001 완료 후 삭제 가능
+/// </summary>
+public partial class TestSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        // 테스트용 빈 시스템 - 컴파일 검증용
+    }
+}
